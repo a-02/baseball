d08b0f3a5b0d206da158fd9a6dd3cd46d0b944e8
nks <me@nk5.nl>
2022-04-26 11:03:31 -0500

    hello

---
+++ index.js
@@ -0,0 +1,55 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const form = document.getElementById('form1');
+    const body = document.getElementById('body');
+    var storageIndex = localStorage.length;
+
+    function renderCard(name, position, teamName) {
+        return `
+	<div class="card">
+		  <img src="https://placekitten.com/500/600" class="kitty">
+		  <div class="bottom">
+		    <div class="bottomleft">
+		      <div class="name">
+		        ${name}
+	      	      </div>
+		      <div class="position">
+		        ${position}
+	              </div>
+		    </div>
+		    <div class="bottomright">
+		      <div class="team">
+		        ${teamName}
+	              </div>
+		    </div>
+		  </div>
+		</div>
+		  `;
+    }
+
+    function cardSubmit(event) {
+        body.insertAdjacentHTML(
+            "beforeend",
+            renderCard(
+                form.name.value,
+                form.position.value,
+                form.team.value
+            )
+        )
+        const cardObject = {
+            name: form.name.value,
+            position: form.position.value,
+            teamName: form.team.value
+        }
+        localStorage.setItem(
+            storageIndex,
+            JSON.stringify(cardObject)
+        )
+        event.preventDefault();
+    };
+
+    form.addEventListener('submit', cardSubmit);
+
+    let secret = "friend";
+
+});
